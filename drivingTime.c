f8b032e2e6e0591223e7a5373c9706036ce02de8
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-07-29 11:35:15 +0530

    Create drivingTime.c

---
+++ drivingTime.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int distance=300;
+  int speed = 75;
+  int time=distance/speed;
+  printf("The driving time from point A to B is %d hr\n",time);
+  return 0;
+}
