cd963ec5bfa316572fa465e25cdef32591b9ad0d
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-07-31 13:58:00 +0530

    Create elseIf.c

---
+++ elseIf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int grade;
+  printf("Enter grade");
+  scanf("%d",&grade);
+  if(grade>=80)
+    printf("Good");
+  else if(grade>=60)
+    printf("Not Bad");
+  else
+    printf("Try Again");
+  return 0;
+}
