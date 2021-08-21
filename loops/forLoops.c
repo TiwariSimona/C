bc49db559241bb245df32b9230019b934d43b3e9
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-21 10:34:18 +0530

    Update forLoops.c
    1. Specify 'for' statement
    2. Specify 'for' sections.
         -'Initialization' statement
         - 'Loop condition'
        - 'Update' statement
    3. Loop body

--- loops/forLoops.c
+++ loops/forLoops.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include<stdlib.h>
+int manin(){
+  int num;
+  int i;
+  printf("Enter a number");
+  scanf("%d",&num);
+  for(i=1;i<num;i++){
+    printf("%d",i);
+  }
+  return 0;
+}
