40da5634d008d5714d1471812bb53c72c6807104
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-21 10:43:15 +0530

    Update absoluteNumber.c

--- absoluteNumber.c
+++ absoluteNumber.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include <math.h>
+int main(){
+ int num;
+  printf("Enter positive/negative number");
+  scanf("%d",&num);
+  if(num<0)
+    printf("Absolute number of %d is %d \n",num,(-1)num);
+  else
+    printf("Absolute number of %d is %d \n",num,num);
+  return 0;
+}
