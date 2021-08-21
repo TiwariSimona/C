1956a5823eb94c6ca7ee645f62c0109bda47067b
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-21 11:49:56 +0530

    Update clockRepresentation.c

--- clockRepresentation.c
+++ clockRepresentation.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
+int main(){
+int Tseconds;
+int hours,minutes,Rseconds;
+  printf("Enter seconds");
+  scanf("%d",&Tseconds);
+  hours=Tseconds/3600;
+  minutes=(Tseconds-hours*3600)/60;
+  Rseconds=(Tseconds-hours*3600)*60;
+  printf(%d:%d:%d \n",hours,minutes,Rseconds)
