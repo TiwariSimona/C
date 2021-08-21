44c8324f45ef0468aa223619a710ec2dbbf11e85
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-21 10:29:27 +0530

    Update doWhileLoops.c

--- loops/doWhileLoops.c
+++ loops/doWhileLoops.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include<stdlib.h>
+int main(){
+  int price,totalprice=0;
+  do{
+    printf("Enter price");
+    scanf("%d",&price);
+    totalprice=totalprice+price;
+  }while(price!=0);
+  printf("totalprice=%d",totalprice);
+  return 0;
+}
