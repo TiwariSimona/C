50089d638ec9a58ac7ce4e2f0bb41f92a2cba65d
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-24 10:33:19 +0530

    Update leapYear.c

--- leapYear.c
+++ leapYear.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include<stdlib.h>
+int main(){
+  int year;
+  printf("Enter year");
+  scanf("%d",&year);
+  if(year%400==0)
+    printf("Leap Year);
+  else if(year%100==0)
+     printf("Not  leap year");
+  else if(year%4==0)
+     printf("Leap Year");
+  else
+     printf("Not a leap year");
+ return 0;
+           }
+         
