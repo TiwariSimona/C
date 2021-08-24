a72e8fcdad80f9518c3168c4514018df3589bff9
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-08-24 10:39:20 +0530

    Update pattern1.c
    1
    12
    123
    1234
    12345

--- loops/nestedLoops.c/pattern1.c
+++ loops/nestedLoops.c/pattern1.c
@@ -1,1 +1,14 @@
-#
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int i,j;
+  int i;
+  printf("Enter 'n'");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++)
+      printf("%d",j);
+    printf("\n");
+  }
+  return 0;
+}
