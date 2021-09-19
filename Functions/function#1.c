51cbed4ae506d135bc8ff94632a2795c2acfb9d6
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-09-19 13:51:36 +0530

    Update function#1.c

--- Functions/function#1.c
+++ Functions/function#1.c
@@ -1,1 +1,14 @@
+// Print Greetings
+// no parameter   nothing returned
 
+#include<stdio.h>
+#include<stdlib.h>
+void printGreetings()
+{
+  printf("Hello");
+}
+
+int main(){
+  printGreetings();
+ return 0;
+}
